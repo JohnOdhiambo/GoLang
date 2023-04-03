8200698f8ffa059b8d5ec6797f94476681fa6b1c
JohnOdhiambo <john.oduor.j@gmail.com>
2023-04-03 19:20:13 +0300

    Go program

---
+++ GoLang/go.mod
@@ -0,0 +1,3 @@
+module GoLang/test
+
+go 1.20
---
+++ GoLang/test.go
@@ -0,0 +1,12 @@
+// To enable dependency tracking for your code by creating a go.mod file,
+// run the go mod init command,  giving it the name of the module your code will be in.
+// The name is the module's module path.
+// In our case run go mod init GoLang/test
+package main
+
+// fmt package, which contains functions for formatting text, including printing to the console.
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Jay")
+}
