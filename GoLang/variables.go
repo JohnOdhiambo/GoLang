7651adf6ef0eebf47629f4137ba5249c20583254
JohnOdhiambo <john.oduor.j@gmail.com>
2023-04-03 20:05:48 +0300

    Types of variables in Go

---
+++ GoLang/variables.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num = 22
+	var name = "Jay"
+	var pi = 3.142
+
+	var var1, var2, var3 = 4, "ABC", 22.15
+
+	myvar1 := 40
+	myvar2 := "Jayjay"
+	myvar3 := 12.34
+
+	fmt.Println("The number is: ", num)
+	fmt.Println("The name is: ", name)
+	fmt.Println("The pie is: ", pi)
+	fmt.Println("The different variables declared and printed in one line are: ", var1, var2, var3)
+	fmt.Println("The different way of declaring variables: ", myvar1, myvar2, myvar3)
+
+}
